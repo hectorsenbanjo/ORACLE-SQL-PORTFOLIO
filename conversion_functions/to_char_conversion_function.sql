cbae4d6cc870fb0019ee32847987fd4b7780b134
Hector <hsenbanjo@yahoo.com>
2025-08-06 22:53:01 +0100

    to_char_conversion_function.sql

--- conversion_functions/to_char_conversion_function.sql
+++ conversion_functions/to_char_conversion_function.sql
@@ -1,5 +1,5 @@
 SELECT 
     'Employee ' || first_name || ' ' || last_name || ' earns ' ||
     TO_CHAR(salary, 'L999G999D99') || ' monthly and was hired on the ' ||
-    TO_CHAR(hire_date, 'ddthsp" of " Month yyyy') AS "employee_info"
+    TO_CHAR(hire_date, 'ddthsp" of " Month, yyyy') AS "employee_info"
 FROM employees;

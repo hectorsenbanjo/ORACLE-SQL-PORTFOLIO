435fd2cc25c4c15fbdb15301cab461f678175de3
Hector <hsenbanjo@yahoo.com>
2025-08-07 13:47:47 +0100

    average_salary_by_department.sql

--- group_functions/average_salary_by_department.sql
+++ group_functions/average_salary_by_department.sql
@@ -1,11 +1,12 @@
--- average_salary_by_department.sql
+-- avg_salary_by_department.sql
+-- This query calculates the average salary for each department.
 
 SELECT 
     department_id,
-    ROUND(AVG(salary), 2) AS average_salary
+    ROUND(AVG(salary), 2) AS avg_salary
 FROM 
     employees
 GROUP BY 
     department_id
 ORDER BY 
-    department_id;
+    avg_salary DESC;
